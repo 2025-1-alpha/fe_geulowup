67d88c4f2006a22fad180db6d87eae0237c74baa
Sinji <sinzi031012@naver.com>
2025-04-07 01:55:47 +0900

    feat: 푸터 완성

--- src/app/(layout)/layout.tsx
+++ src/app/(layout)/layout.tsx
@@ -1,11 +1,12 @@
 import Header from '@/components/ui/Header';
+import Footer from '@/components/ui/Footer';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
       {children}
-      <footer></footer>
+      <Footer />
     </>
   );
 }

4a3f75bf3c96bf1092dee112c32d1d6bd7fac47c
Sinji <sinzi031012@naver.com>
2025-04-08 02:40:53 +0900

    feat: 수정 결과 퍼블리싱

--- src/app/(layout)/advice/_components/AdviceResultBox/index.tsx
+++ src/app/(layout)/advice/_components/AdviceResultBox/index.tsx
@@ -9,8 +9,16 @@ export default function AdviceResult({ content }: AdviceResultProps) {
     <section className="border-layout-grey3 bg-layout-grey1 flex h-[684px] w-[520px] flex-col rounded-[7px] border px-3 py-4">
       <p className="title-sm text-layout-grey7">AI 수정 결과</p>
       <Spacing size={12} />
-      <div className="body-md text-layout-grey7 whitespace-pre-wrap">{content}</div>
-      {/* TODO : svg 버튼들 추가하기 */}
+      <div className="border-layout-grey3 relative flex h-[560px] w-full flex-col overflow-y-auto rounded-[6px] border p-3">
+        {content ? (
+          <p className="body-md text-layout-grey7 whitespace-pre-wrap">{content}</p>
+        ) : (
+          <p className="body-md text-layout-grey4 whitespace-pre-wrap">
+            왼쪽에 글을 입력하시면 AI가 수정한 글이 여기에 나타납니다.
+          </p>
+        )}
+      </div>
+      <div className="flex w-full items-end justify-end">{/* TODO : 아이콘 추가하기 */}</div>
     </section>
   );
 }

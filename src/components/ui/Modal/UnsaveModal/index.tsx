099789dc41bf3dfcf9c13c60f19dba2608da9929
Sinji <sinzi031012@naver.com>
2025-05-22 03:20:08 +0900

    feat: 사용 취소 모달 생성

--- src/components/ui/Modal/UnsaveModal/index.tsx
+++ src/components/ui/Modal/UnsaveModal/index.tsx
@@ -1,5 +1,7 @@
 import IconClose from '@/assets/icons/icon-close.svg';
+import IconDoor from '@/assets/icons/icon-door.svg';
 import { Button } from '../../Button';
+import { Spacing } from '../../Spacing';
 import { useUnsaveModalStore } from '@/stores/useUnsaveModalStore';
 import { useModalStore } from '@/stores/useModalStore';
 
@@ -10,14 +12,18 @@ export default function UnsaveModal() {
   if (!isUnsaveOpen) return null;
 
   return (
-    <section className="bg-layout-grey1 flex h-[252px] w-[448px] flex-col rounded-sm p-6">
-      <button>
+    <section className="bg-layout-grey1 flex h-[252px] w-[448px] flex-col items-center justify-center rounded-lg p-6">
+      <button onClick={closeUnsaveModal} className="flex w-full justify-end">
         <IconClose />
       </button>
-      <div></div>
-      <div className="flex">저장하지 않고 나가시겠습니까?</div>
+      <div className="bg-primary-navy4 flex h-[60px] w-[60px] items-center justify-center rounded-full">
+        <IconDoor />
+      </div>
+      <Spacing size={8} />
+      <div className="title-sm text-layout-grey6 flex">저장하지 않고 나가시겠습니까?</div>
+      <Spacing size={38} />
       <section className="flex gap-2">
-        <Button size="xsmall" onClick={closeUnsaveModal}>
+        <Button size="xsmall" state="line" onClick={closeUnsaveModal}>
           취소
         </Button>
         <Button

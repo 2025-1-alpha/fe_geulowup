f258db06f6c8dc2edcbb4b59c2ac1a128c20d707
Sinji <sinzi031012@naver.com>
2025-05-29 03:38:48 +0900

    chore: 디버깅용 콘솔 제거

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -2,14 +2,13 @@ import { checkAuth } from '@/utils/checkAuth';
 import { useLoginModalStore } from '@/stores/useLoginModal';
 
 export const useAuth = () => {
-  const { isLoginOpen, openLoginModal } = useLoginModalStore();
+  const {  openLoginModal } = useLoginModalStore();
 
   const requireAuth = (): boolean => {
     const isAuthenticated = checkAuth();
 
     if (!isAuthenticated) {
       openLoginModal();
-      console.log(isLoginOpen)
       return false;
     }
 

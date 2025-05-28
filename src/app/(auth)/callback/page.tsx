7b055b0d9a9e9b1efd241d4fd8d29e2d80405d31
Sinji <sinzi031012@naver.com>
2025-05-28 17:51:42 +0900

    feat: 소셜 로그인 API 연결

--- src/app/(auth)/callback/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function CallbackPage() {
-  return <div>콜백 페이지</div>;
-}
--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,22 +1,15 @@
 import { ReactNode } from 'react';
 
 type Props = {
-  children: ReactNode;
   image: ReactNode;
   content: ReactNode;
 };
 
-export default function AuthLayout({ children, content, image }: Props) {
+export default function AuthLayout({ content, image }: Props) {
   return (
     <div className="flex min-h-screen">
-      {children ? (
-        <div className="flex w-full items-center justify-center">{children}</div>
-      ) : (
-        <>
-          <div className="flex w-1/2 items-center justify-center">{content}</div>
-          <div className="relative w-1/2">{image}</div>
-        </>
-      )}
+      <div className="flex w-1/2 items-center justify-center">{content}</div>
+      <div className="relative w-1/2">{image}</div>
     </div>
   );
 }

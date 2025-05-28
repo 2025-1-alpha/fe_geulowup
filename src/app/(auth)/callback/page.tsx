b6acb960af6a453fd56e262e047fb71a651389f0
Sinji <sinzi031012@naver.com>
2025-05-28 16:26:39 +0900

    feat: 콜백 페이지 생성

---
+++ src/app/(auth)/callback/page.tsx
@@ -0,0 +1,3 @@
+export default function CallbackPage() {
+  return <div>콜백 페이지</div>;
+}
--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,16 +1,22 @@
 import { ReactNode } from 'react';
 
 type Props = {
+  children: ReactNode;
   image: ReactNode;
   content: ReactNode;
 };
 
-export default function AuthLayout({ content, image }: Props) {
+export default function AuthLayout({ children, content, image }: Props) {
   return (
     <div className="flex min-h-screen">
-      <div className="flex w-1/2 items-center justify-center">{content}</div>
-
-      <div className="relative w-1/2">{image}</div>
+      {children ? (
+        <div className="flex w-full items-center justify-center">{children}</div>
+      ) : (
+        <>
+          <div className="flex w-1/2 items-center justify-center">{content}</div>
+          <div className="relative w-1/2">{image}</div>
+        </>
+      )}
     </div>
   );
 }
